31862c38f1bf0e2b8498da500d801e291f4e6ce4
Dean DU TOIT <ddu-toit@e4r25p3.jnb.42.fr>
2017-06-07 15:07:47 +0200

    nib.hpp

---
+++ includes/nibbler.hpp
@@ -0,0 +1,32 @@
+//
+// Created by Dean DU TOIT on 2017/06/07.
+//
+
+#pragma once
+
+#include <iostream>
+#include <dlfcn.h>
+#include <string>
+#include <vector>
+#include <SDL2/SDL.h>
+#include "../lib/IGUI.hpp"
+
+class LibLoadFailed : public std::exception
+{
+private:
+	std::string _err;
+public:
+
+	LibLoadFailed(std::string err) : _err(err)
+	{
+	}
+
+	virtual const char	*what() const throw()
+	{
+		std::string _errRet;
+
+		_errRet.append("Library Load Failed : ");
+		_errRet.append(_err);
+		return (_errRet.c_str());
+	}
+};
