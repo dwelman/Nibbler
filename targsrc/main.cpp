70fbd2a8d22097b9187794f984c3d159ac273b06
Dean DU TOIT <ddu-toit@e4r25p3.jnb.42.fr>
2017-06-07 15:10:44 +0200

    main changes

--- targsrc/main.cpp
+++ targsrc/main.cpp
@@ -1,12 +1,72 @@
-#include <iostream>
-#include <string>
-#include "Snake.hpp"
+#include <nibbler.hpp>
+
+void	*getHandle(const char *lib)
+{
+	void 	*handle;
+
+	handle = dlopen (lib, RTLD_LAZY);
+	if (!handle)
+		throw (LibLoadFailed(dlerror()));
+	return (handle);
+}
+
+IGUI	*loadLibObject(void *handle)
+{
+	void	*make;
+	char 	*error;
+	IGUI* 	guiObject = nullptr;
+
+	make = dlsym(handle, "make");
+	auto my_gui = reinterpret_cast<IGUI *(*)(void)>(make);
+	if ((error = dlerror()) != NULL)
+	{
+		throw (LibLoadFailed(error));
+	}
+	if (my_gui != nullptr)
+		guiObject = my_gui();
+	return (guiObject);
+}
+
+void	gameLoop(const std::string &startingLib, int x, int y)
+{
+	bool 							is_running = true;
+	s_keypress						keys;
+	IGUI							*guiLib;
+	void							*handle;
+	std::vector<DrawableObj>		drawObj;
+
+	handle = getHandle(startingLib.c_str());
+	guiLib = loadLibObject(handle);
+	guiLib->start();
+	guiLib->setSize(x, y);
+	DrawableObj temp;
+	temp.y = 0;
+	temp.x = 0;
+	drawObj.push_back(temp);
+	while (is_running)
+	{
+		if (guiLib->getInput(keys) > 0)
+		{
+			std::cout << keys.p1north << " " << keys.p1south << " "
+					  << keys.p1west << " " << keys.p1east << std::endl;
+			drawObj[0].x -= keys.p1east;
+			drawObj[0].x += keys.p1west;
+			drawObj[0].y -= keys.p1north;
+			drawObj[0].y += keys.p1south;
+		}
+		guiLib->drawObjects(drawObj);
+
+		//DO something with keys
+		if (keys.quit > 0)
+			is_running = false;
+	}
+}
 
 int main(int argc, char **argv)
 {
-    int         mapWidth;
-    int         mapHeight;
-    std::string startingLib;
+    int         			mapWidth;
+    int        				mapHeight;
+    std::string 			startingLib;
 
     if (argc < 3 || argc > 4)
     {
@@ -50,16 +110,16 @@ int main(int argc, char **argv)
     }
     else
     {
-        //srand(time(NULL));
-
+        srand(time(NULL));
     }
-    Snake snake(mapWidth / 2, mapHeight / 2, SnakeSegment::NORTH);
-    snake.PrintSnake();
-    snake.UpdateSnake();
-    snake.PrintSnake();
-    snake.ChangeSnakeHeadDirection(SnakeSegment::EAST);
-    snake.UpdateSnake();
-    snake.PrintSnake();
-    snake.UpdateSnake();
-    snake.PrintSnake();
+
+	try
+	{
+		gameLoop(startingLib, mapWidth, mapHeight);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+		return (-1);
+	}
 }
